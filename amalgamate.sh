33cd99281b034a9a46c75530832004384ee052ef
J. Protopopovs <jprotopopov1122@gmail.com>
2019-04-21 18:39:17 +0300

    Add amalgamation script

---
+++ amalgamate.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# This script uses the tool Amalgamate from https://github.com/vinniefalco/Amalgamate/
+
+LUACPPB_DIR=$1
+DEST_DIR=$2
+AMALGAMATE=$3
+
+if [[ "$2" == "" ]]; then
+	echo "Usage: $0 'LuaCppB root' 'destination directory' [amalgamate tool path]"
+	exit
+fi
+
+if [[ "$3" == "" ]]; then
+	AMALGAMATE=amalgamate
+fi
+
+amalgamate=$(which "$AMALGAMATE")
+if [[ -x "$amalgamate" ]] ; then
+	echo "Install amalgamate from https://github.com/vinniefalco/Amalgamate/"
+fi
+
+find "$LUACPPB_DIR/source" -name "*.cpp" -exec cat {} \; > "$DEST_DIR/LuaCppB.raw.cpp"
+$amalgamate -i "$LUACPPB_DIR/headers" "$DEST_DIR/LuaCppB.raw.cpp" "$DEST_DIR/LuaCppB.cpp"
+$amalgamate -i "$LUACPPB_DIR/headers" "$LUACPPB_DIR/headers/luacppb/LuaCppB.h" "$DEST_DIR/LuaCppB.h"
+rm "$DEST_DIR/LuaCppB.raw.cpp"
